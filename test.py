35dd115d0297422af6bbe65a69d32b0597d705c0
frdel <38891707+frdel@users.noreply.github.com>
2024-06-11 00:48:34 +0200

    readme update

--- test.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def execute_terminal_command(command, input_data=None):
-    result = subprocess.run(command, shell=True, capture_output=True, text=True, input=input_data)
-    return result.stdout + result.stderr
-
-print("1",execute_terminal_command("ls", "y\n"))
-print("2",execute_terminal_command("ls", None))
