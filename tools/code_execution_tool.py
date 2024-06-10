c3b6172538ea3c23c9111b6b65f7c4455ca0ca1e
frdel <38891707+frdel@users.noreply.github.com>
2024-06-10 23:08:32 +0200

    terminal and nodejs

--- tools/code_execution_tool.py
+++ tools/code_execution_tool.py
@@ -1,34 +1,42 @@
-import os, json, contextlib
+import os, json, contextlib, subprocess, ast, shlex
 from io import StringIO
 from tools.helpers import files
-import ast
 from agent import Agent
 
 
-def execute(agent:Agent ,code_text:str, **kwargs):
+def execute(agent:Agent , code_text:str, runtime:str, **kwargs):
 
-    return execute_user_code(code_text)
+    os.chdir(files.get_abs_path("./work_dir")) #change CWD to work_dir
     
-    def execute_code(code_string, input=None):
-        os.chdir(files.get_abs_path("./work_dir")) #change CWD to work_dir
-        buffer = StringIO()
-        local_vars = {} # {"input": input}
+    if runtime == "python":
+        return execute_python_code(code_text)
+    elif runtime == "nodejs":
+        return execute_nodejs_code(code_text)
+    elif runtime == "terminal":
+        return execute_terminal_command(code_text)
+    else:
+        return files.read_file("./prompts/fw.code_runtime_wrong.md", runtime=runtime)
+    
+    # def execute_code(code_string, input=None):
+    #     os.chdir(files.get_abs_path("./work_dir")) #change CWD to work_dir
+    #     buffer = StringIO()
+    #     local_vars = {} # {"input": input}
 
-        with contextlib.redirect_stdout(buffer):
-            try:        
-                indented_code = "\n    ".join(code_string.strip().split("\n"))
-                wrapped_code = f"""def isolate(input):\n    {indented_code}"""        
-                exec(wrapped_code, None, local_vars) # exec(code_string, {"__builtins__": __builtins__}, local_vars)
-                return local_vars.get('isolate', lambda: None)(input)  # type: ignore # calling main if defined to get its return value
+    #     with contextlib.redirect_stdout(buffer):
+    #         try:        
+    #             indented_code = "\n    ".join(code_string.strip().split("\n"))
+    #             wrapped_code = f"""def isolate(input):\n    {indented_code}"""        
+    #             exec(wrapped_code, None, local_vars) # exec(code_string, {"__builtins__": __builtins__}, local_vars)
+    #             return local_vars.get('isolate', lambda: None)(input)  # type: ignore # calling main if defined to get its return value
 
-            except Exception as e:
-                import traceback
-                error_info = traceback.format_exc()
-                return json.dumps({"error": str(e), "details": error_info})
-        # return local_vars.get("output", buffer.getvalue())
+    #         except Exception as e:
+    #             import traceback
+    #             error_info = traceback.format_exc()
+    #             return json.dumps({"error": str(e), "details": error_info})
+    #     # return local_vars.get("output", buffer.getvalue())
 
-    result = json.dumps(output) if (output := execute_code(code_text)) else files.read_file("./prompts/fw.code_no_output.md")
-    return result
+    # result = json.dumps(output) if (output := execute_code(code_text)) else files.read_file("./prompts/fw.code_no_output.md")
+    # return result
 
 
 
@@ -70,13 +78,13 @@ def wrap_code_with_return_and_function(code):
     # Convert the AST back to source code
     wrapped_code = compile(module, filename="<ast>", mode="exec")
     return wrapped_code
-def execute_user_code(code):
+
+def execute_python_code(code):
     try:
         wrapped_code = wrap_code_with_return_and_function(code)
         exec_globals = {}
         exec_locals = {}
         exec(wrapped_code, exec_globals, exec_locals)
-        os.chdir(files.get_abs_path("./work_dir")) #change CWD to work_dir
         try:
             return exec_locals.get('isolate', lambda: None)()
         except Exception as e:
@@ -86,3 +94,14 @@ def execute_user_code(code):
 
     except Exception as e:
         return "Error: " + str(e)
+
+def execute_nodejs_code(code):
+    # Ensure code is properly escaped
+    escaped_code = shlex.quote(code)
+    process = subprocess.Popen(['node', '-e', escaped_code], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    stdout, stderr = process.communicate()
+    return stdout, stderr
+
+def execute_terminal_command(command):
+    result = subprocess.run(command, shell=True, text=True, capture_output=True)
+    return result.stdout + result.stderr
