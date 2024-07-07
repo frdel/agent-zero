89aa3b23015088b5fac4b11eba1c0bd8b88745d6
frdel <38891707+frdel@users.noreply.github.com>
2024-07-07 14:28:58 +0200

    SSH output cleanup, delegation fix

--- python/tools/call_subordinate.py
+++ python/tools/call_subordinate.py
@@ -8,10 +8,7 @@ class Delegation(Tool):
     def execute(self, message="", reset="", **kwargs):
         # create subordinate agent using the data object on this agent and set superior agent to his data object
         if self.agent.get_data("subordinate") is None or str(reset).lower().strip() == "true":
-            # subordinate = Agent(system_prompt=self.agent.system_prompt, tools_prompt=self.agent.tools_prompt, number=self.agent.number+1)
-            config = self.agent.__dict__.copy()
-            config["agent_number"] = self.agent.number+1
-            subordinate = Agent(**config)
+            subordinate = Agent(self.agent.number+1, self.agent.config)
             subordinate.set_data("superior", self.agent)
             self.agent.set_data("subordinate", subordinate) 
         # run subordinate agent message loop
