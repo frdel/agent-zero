4307dce2c8f51bcbbb4228be0f6b2a91476fcc30
frdel <38891707+frdel@users.noreply.github.com>
2024-09-12 22:43:15 +0200

    Bugfixes
    Ollama context and chat model fix
    Endpoints in .env
    defer.py event loop fix
    Error logging for ui fix

--- models.py
+++ models.py
@@ -2,11 +2,12 @@ import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI, OpenAI, OpenAIEmbeddings, AzureChatOpenAI, AzureOpenAIEmbeddings, AzureOpenAI
 from langchain_community.llms.ollama import Ollama
+from langchain_ollama import ChatOllama
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_anthropic import ChatAnthropic
 from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_google_genai import ChatGoogleGenerativeAI, HarmBlockThreshold, HarmCategory
+from langchain_google_genai import GoogleGenerativeAI, HarmBlockThreshold, HarmCategory
 from pydantic.v1.types import SecretStr
 
 
@@ -22,11 +23,12 @@ def get_api_key(service):
 
 
 # Ollama models
-def get_ollama_chat(model_name:str, temperature=DEFAULT_TEMPERATURE, base_url="http://localhost:11434"):
-    return Ollama(model=model_name,temperature=temperature, base_url=base_url)
+def get_ollama_chat(model_name:str, temperature=DEFAULT_TEMPERATURE, base_url=os.getenv("OLLAMA_BASE_URL") or "http://127.0.0.1:11434", num_ctx=8192):
+    return ChatOllama(model=model_name,temperature=temperature, base_url=base_url, num_ctx=num_ctx)
 
-def get_ollama_embedding(model_name:str, temperature=DEFAULT_TEMPERATURE):
-    return OllamaEmbeddings(model=model_name,temperature=temperature)
+def get_ollama_embedding(model_name:str, temperature=DEFAULT_TEMPERATURE, base_url=os.getenv("OLLAMA_BASE_URL") or "http://127.0.0.1:11434"):
+    
+    return OllamaEmbeddings(model=model_name,temperature=temperature, base_url=base_url)
 
 # HuggingFace models
 
@@ -34,63 +36,46 @@ def get_huggingface_embedding(model_name:str):
     return HuggingFaceEmbeddings(model_name=model_name)
 
 # LM Studio and other OpenAI compatible interfaces
-def get_lmstudio_chat(model_name:str, base_url="http://localhost:1234/v1", temperature=DEFAULT_TEMPERATURE):
+def get_lmstudio_chat(model_name:str, temperature=DEFAULT_TEMPERATURE, base_url=os.getenv("LM_STUDIO_BASE_URL") or "http://127.0.0.1:1234/v1"):
     return ChatOpenAI(model_name=model_name, base_url=base_url, temperature=temperature, api_key="none") # type: ignore
 
-def get_lmstudio_embedding(model_name:str, base_url="http://localhost:1234/v1"):
+def get_lmstudio_embedding(model_name:str, base_url=os.getenv("LM_STUDIO_BASE_URL") or "http://127.0.0.1:1234/v1"):
     return OpenAIEmbeddings(model_name=model_name, base_url=base_url) # type: ignore
 
 # Anthropic models
-def get_anthropic_chat(model_name:str, api_key=None, temperature=DEFAULT_TEMPERATURE):
-    api_key = api_key or get_api_key("anthropic")
+def get_anthropic_chat(model_name:str, api_key=get_api_key("anthropic"), temperature=DEFAULT_TEMPERATURE):
     return ChatAnthropic(model_name=model_name, temperature=temperature, api_key=api_key) # type: ignore
 
 # OpenAI models
-def get_openai_chat(model_name:str, api_key=None, temperature=DEFAULT_TEMPERATURE):
-    api_key = api_key or get_api_key("openai")
+def get_openai_chat(model_name:str, api_key=get_api_key("openai"), temperature=DEFAULT_TEMPERATURE):
     return ChatOpenAI(model_name=model_name, temperature=temperature, api_key=api_key) # type: ignore
 
-def get_openai_instruct(model_name:str,api_key=None, temperature=DEFAULT_TEMPERATURE):
-    api_key = api_key or get_api_key("openai")
+def get_openai_instruct(model_name:str, api_key=get_api_key("openai"), temperature=DEFAULT_TEMPERATURE):
     return OpenAI(model=model_name, temperature=temperature, api_key=api_key) # type: ignore
 
-def get_openai_embedding(model_name:str, api_key=None):
-    api_key = api_key or get_api_key("openai")
+def get_openai_embedding(model_name:str, api_key=get_api_key("openai")):
     return OpenAIEmbeddings(model=model_name, api_key=api_key) # type: ignore
 
-def get_azure_openai_chat(deployment_name:str, api_key=None, temperature=DEFAULT_TEMPERATURE, azure_endpoint=None):
-    api_key = api_key or get_api_key("openai_azure")
-    azure_endpoint = azure_endpoint or os.getenv("OPENAI_AZURE_ENDPOINT")
+def get_azure_openai_chat(deployment_name:str, api_key=get_api_key("openai_azure"), temperature=DEFAULT_TEMPERATURE, azure_endpoint=os.getenv("OPENAI_AZURE_ENDPOINT")):
     return AzureChatOpenAI(deployment_name=deployment_name, temperature=temperature, api_key=api_key, azure_endpoint=azure_endpoint) # type: ignore
 
-def get_azure_openai_instruct(deployment_name:str, api_key=None, temperature=DEFAULT_TEMPERATURE, azure_endpoint=None):
-    api_key = api_key or get_api_key("openai_azure")
-    azure_endpoint = azure_endpoint or os.getenv("OPENAI_AZURE_ENDPOINT")
+def get_azure_openai_instruct(deployment_name:str, api_key=get_api_key("openai_azure"), temperature=DEFAULT_TEMPERATURE, azure_endpoint=os.getenv("OPENAI_AZURE_ENDPOINT")):
     return AzureOpenAI(deployment_name=deployment_name, temperature=temperature, api_key=api_key, azure_endpoint=azure_endpoint) # type: ignore
 
-def get_azure_openai_embedding(deployment_name:str, api_key=None, azure_endpoint=None):
-    api_key = api_key or get_api_key("openai_azure")
-    azure_endpoint = azure_endpoint or os.getenv("OPENAI_AZURE_ENDPOINT")
+def get_azure_openai_embedding(deployment_name:str, api_key=get_api_key("openai_azure"), azure_endpoint=os.getenv("OPENAI_AZURE_ENDPOINT")):
     return AzureOpenAIEmbeddings(deployment_name=deployment_name, api_key=api_key, azure_endpoint=azure_endpoint) # type: ignore
 
 # Google models
-def get_google_chat(model_name:str, api_key=None, temperature=DEFAULT_TEMPERATURE):
-    api_key = api_key or get_api_key("google")
-    return ChatGoogleGenerativeAI(model=model_name, temperature=temperature, google_api_key=api_key, safety_settings={HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE }) # type: ignore
+def get_google_chat(model_name:str, api_key=get_api_key("google"), temperature=DEFAULT_TEMPERATURE):
+    return GoogleGenerativeAI(model=model_name, temperature=temperature, google_api_key=api_key, safety_settings={HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE }) # type: ignore
 
 # Groq models
-def get_groq_chat(model_name:str, api_key=None, temperature=DEFAULT_TEMPERATURE):
-    api_key = api_key or get_api_key("groq")
+def get_groq_chat(model_name:str, api_key=get_api_key("groq"), temperature=DEFAULT_TEMPERATURE):
     return ChatGroq(model_name=model_name, temperature=temperature, api_key=api_key) # type: ignore
    
 # OpenRouter models
-def get_openrouter(model_name: str="meta-llama/llama-3.1-8b-instruct:free", api_key=None, temperature=DEFAULT_TEMPERATURE):
-    api_key = api_key or get_api_key("openrouter")
-    return ChatOpenAI(api_key=api_key, base_url="https://openrouter.ai/api/v1", model=model_name, temperature=temperature) # type: ignore
-        
-def get_embedding_hf(model_name="sentence-transformers/all-MiniLM-L6-v2"):
-    return HuggingFaceEmbeddings(model_name=model_name)
-
-def get_embedding_openai(api_key=None):
-    api_key = api_key or get_api_key("openai")
-    return OpenAIEmbeddings(api_key=api_key) #type: ignore
+def get_openrouter_chat(model_name: str, api_key=get_api_key("openrouter"), temperature=DEFAULT_TEMPERATURE, base_url=os.getenv("OPEN_ROUTER_BASE_URL") or "https://openrouter.ai/api/v1"):
+    return ChatOpenAI(api_key=api_key, model=model_name, temperature=temperature, base_url=base_url) # type: ignore
+      
+def get_openrouter_embedding(model_name: str, api_key=get_api_key("openrouter"), base_url=os.getenv("OPEN_ROUTER_BASE_URL") or "https://openrouter.ai/api/v1"):
+    return OpenAIEmbeddings(model=model_name, api_key=api_key, base_url=base_url) # type: ignore
