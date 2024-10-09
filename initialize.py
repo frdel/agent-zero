de70694ad06fd9585547cbb153830e46ea75158e
bpawnzZ <bpawnz@yandex.com>
2024-10-09 10:40:04 -0400

    change dockereHub to run

--- initialize.py
+++ initialize.py
@@ -1,5 +1,3 @@
-
-
 def initialize():
     
     # main chat model used by agents (smarter, more accurate)
@@ -13,7 +11,7 @@ def initialize():
     # chat_llm = models.get_groq_chat(model_name="llama-3.1-70b-versatile", temperature=0)
     
     # utility model used for helper functions (cheaper, faster)
-    utility_llm = chat_llm # change if you want to use a different utility model
+    utility_llm = chat_llm  # change if you want to use a different utility model
 
     # embedding model used for memory
     # embedding_llm = models.get_openai_embedding(model_name="text-embedding-3-small")
@@ -22,33 +20,33 @@ def initialize():
 
     # agent configuration
     config = AgentConfig(
-        chat_model = chat_llm,
-        utility_model = utility_llm,
-        embeddings_model = embedding_llm,
+        chat_model=chat_llm,
+        utility_model=utility_llm,
+        embeddings_model=embedding_llm,
         # prompts_subdir = "",
-        # memory_subdir = "",
+        memory_subdir="agents/agent0/memory",
         # knowledge_subdir: str = ""
-        auto_memory_count = 0,
+        auto_memory_count=0,
         # auto_memory_skip = 2,
         # rate_limit_seconds = 60,
-        rate_limit_requests = 15,
+        rate_limit_requests=15,
         # rate_limit_input_tokens = 0,
         # rate_limit_output_tokens = 0,
         # msgs_keep_max = 25,
         # msgs_keep_start = 5,
         # msgs_keep_end = 10,
-        max_tool_response_length = 6000,
-        #response_timeout_seconds = 60,
-        code_exec_docker_enabled = True,
-        code_exec_docker_name = "agent-zero",
-        code_exec_docker_image = "bpawnzz/agent-zero:v2",
-        # code_exec_docker_ports = { "22/tcp": 50022 }
-        # code_exec_docker_volumes = { files.get_abs_path("work_dir"): {"bind": "/root", "mode": "rw"} }
-        code_exec_ssh_enabled = True,
-        code_exec_ssh_addr = "localhost",
-        code_exec_ssh_port = 50022,
-        code_exec_ssh_user = "root",
-        code_exec_ssh_pass = "toor",
+        max_tool_response_length=6000,
+        response_timeout_seconds=60,
+        code_exec_docker_enabled=True,
+        code_exec_docker_name="agent-zero",
+        code_exec_docker_image="bpawnzz/agent-zero:current",
+        code_exec_docker_ports={"22/tcp": 50022},  # Added missing comma here
+        code_exec_docker_volumes={files.get_abs_path("work_dir"): {"bind": "/root", "mode": "rw"}},  # Added missing comma here
+        code_exec_ssh_enabled=True,
+        code_exec_ssh_addr="localhost",
+        code_exec_ssh_port=50022,
+        code_exec_ssh_user="root",
+        code_exec_ssh_pass="toor",
         # additional = {},
     )
 
