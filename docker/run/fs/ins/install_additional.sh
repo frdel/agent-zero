ee4b647916698cdc80c4ef1e54e3ce8500eac4fc
frdel <38891707+frdel@users.noreply.github.com>
2025-03-02 21:11:20 +0100

    HE docker fix + visuals
    docker build fix for playwright
    red accent

--- docker/run/fs/ins/install_additional.sh
+++ docker/run/fs/ins/install_additional.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # install playwright
-bash /ins/install_playwright.sh "$@"
+# bash /ins/install_playwright.sh "$@"
+#moved to post_install.sh to respect the playwright version from requirements.txt
 
 # searxng
 bash /ins/install_searxng.sh "$@"
