fbcb579a91937d436f18123dfa22bdfb156bef84
Rafael Uzarowski <uzarowski.rafael@proton.me>
2025-03-20 23:22:27 +0100

    feat: container process supervision with sueprvisord

--- docker/run/fs/ins/pre_install.sh
+++ docker/run/fs/ins/pre_install.sh
@@ -11,7 +11,9 @@ apt-get update && apt-get install -y \
     curl \
     wget \
     git \
-    ffmpeg 
+    ffmpeg \
+    supervisor \
+    cron
 
 # # Configure system alternatives so that /usr/bin/python3 points to Python 3.12
 # sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.12 1
