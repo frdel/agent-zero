cab7a00c370e7127277724ead020d9522936ca8f
frdel <38891707+frdel@users.noreply.github.com>
2025-05-06 21:03:02 +0200

    npm amd64 conflict fix

--- docker/run/fs/ins/pre_install.sh
+++ docker/run/fs/ins/pre_install.sh
@@ -3,12 +3,15 @@
 # fix permissions for cron files
 chmod 0644 /etc/cron.d/*
 
+echo "=====BEFORE UPDATE====="
+
 # Update and install necessary packages
-apt-get update && apt-get install -y \
+apt clean
+apt-get update && apt-get upgrade -y && apt-get install -y \
     python3 \
     python3-venv \
+    python3-pip \
     nodejs \
-    npm \
     openssh-server \
     sudo \
     curl \
@@ -17,6 +20,13 @@ apt-get update && apt-get install -y \
     ffmpeg \
     supervisor \
     cron
+
+# for some reason npm conflicts with node on amd64 in this version and has to be installed separately
+apt-get install -y \
+    npm 
+    
+
+echo "=====AFTER UPDATE====="
 
 # # Configure system alternatives so that /usr/bin/python3 points to Python 3.12
 # sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.12 1
@@ -27,8 +37,8 @@ apt-get update && apt-get install -y \
 # if [ -f /usr/bin/pip3.12 ]; then
 #   sudo ln -sf /usr/bin/pip3.12 /usr/bin/pip3
 # else
-  python3 -m ensurepip --upgrade
-  python3 -m pip install --upgrade pip
+#   python3 -m ensurepip --upgrade
+#   python3 -m pip install --upgrade pip
 # fi
 
 # Prepare SSH daemon
