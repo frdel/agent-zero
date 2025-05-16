97c60962d4fb4b74d3f503f05191fee7b0d4e69b
Rafael Uzarowski <uzarowski.rafael@proton.me>
2025-05-16 17:19:29 +0200

    feat: Deduplicate and optimize memory similarity threshold

--- python/tools/memory_delete.py
+++ python/tools/memory_delete.py
@@ -1,12 +1,13 @@
 from python.helpers.memory import Memory
 from python.helpers.tool import Tool, Response
 
-class MemoryForget(Tool):
+
+class MemoryDelete(Tool):
 
     async def execute(self, ids="", **kwargs):
         db = await Memory.get(self.agent)
         ids = [id.strip() for id in ids.split(",") if id.strip()]
         dels = await db.delete_documents_by_ids(ids=ids)
 
-        result =  self.agent.read_prompt("fw.memories_deleted.md", memory_count=len(dels))
+        result = self.agent.read_prompt("fw.memories_deleted.md", memory_count=len(dels))
         return Response(message=result, break_loop=False)
