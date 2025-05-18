2991932018e7f5778165dc739838dc92caa6b896
deci <deciduusleaf@gmail.com>
2025-05-18 16:50:35 -0500

    New Instrument: Youtube Transcript retrieval.

---
+++ instruments/default/yt_transcript/yt_transcript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install required Python packages
+pip install youtube_transcript_api
+
+# Run the Python script with the provided URL
+python /a0/instruments/default/yt_transcript/yt_transcript.py "$1" 
