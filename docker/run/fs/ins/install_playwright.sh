c1f548bb1d42772ad2ee7dc9fa561dd3b642d7a8
frdel <38891707+frdel@users.noreply.github.com>
2025-05-30 22:09:07 +0200

    Update install_playwright.sh

--- docker/run/fs/ins/install_playwright.sh
+++ docker/run/fs/ins/install_playwright.sh
@@ -9,7 +9,7 @@ pip install playwright
 # install chromium with dependencies
 # for kali-based
 # if [ "$@" = "hacking" ]; then
-    apt-get install -y fonts-unifont libnss3 libnspr4 libatk1.0-0 libatspi2.0-0 libxcomposite1 libxdamage1      
+    apt-get install -y fonts-unifont libnss3 libnspr4 libatk1.0-0 libatspi2.0-0 libxcomposite1 libxdamage1 libatk-bridge2.0-0    
     playwright install chromium-headless-shell
 # else
 #     # for debian based
