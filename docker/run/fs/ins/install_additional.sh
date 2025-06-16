30cc5a1c9997c85ca6329ec3d0a6429a2a64b2b4
deci <deciduusleaf@gmail.com>
2025-06-16 16:38:57 -0500

    fix: run apt-get update before install to prevent 404 errors on package fetch

--- docker/run/fs/ins/install_additional.sh
+++ docker/run/fs/ins/install_additional.sh
@@ -5,3 +5,9 @@
 
 # searxng - moved to base image
 # bash /ins/install_searxng.sh "$@"
+
+#TODO : move to base image
+
+apt-get update
+apt-get install -y --fix-missing --no-install-recommends \
+    tesseract-ocr-all poppler-utils
