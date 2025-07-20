ac51f419530e4446ad8d4a713cebef235136ea41
frdel <38891707+frdel@users.noreply.github.com>
2025-07-20 22:54:15 +0200

    Update response.py

--- python/tools/response.py
+++ python/tools/response.py
@@ -4,7 +4,7 @@ from python.helpers.tool import Tool, Response
 class ResponseTool(Tool):
 
     async def execute(self, **kwargs):
-        return Response(message=self.args["message"] if "message" in self.args else self.args["text"], break_loop=True)
+        return Response(message=self.args["text"] if "text" in self.args else self.args["message"], break_loop=True)
 
     async def before_execution(self, **kwargs):
         # self.log = self.agent.context.log.log(type="response", heading=f"{self.agent.agent_name}: Responding", content=self.args.get("text", ""))
